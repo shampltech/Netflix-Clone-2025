30a352cd6728264d909ef81a8e7e819e854dc3b8
Mohammed <mamegit2020@gmail.com>
2025-02-11 09:20:56 +0300

    banner page

--- netflix-clone/src/Components/Pages/Home/Home.jsx
+++ netflix-clone/src/Components/Pages/Home/Home.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import Header from '../../Header/Header'
 import Footer from '../../Footer/Footer'
+import Banner from '../../Banner/Banner'
 
 function Home() {
   return (
     <div>
       <Header/>
+      <Banner/>
+      <br/>
       <Footer/>
     </div>
   )
